f8e263f0cb4fc9113e9f1f00e839c768801f596a
Jair130 <2021231@student.cct.ie>
2022-12-28 16:40:31 +0000

    scripts

---
+++ Js/scripts.js
@@ -0,0 +1,5 @@
+/*!
+* Start Bootstrap - Bare v5.0.8 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
